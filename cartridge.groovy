2f2fb4adbd007c161ef28c408edded387da4397e
amiel afurong <amiel.c.l.afurong@accenture.com>
2017-06-06 19:13:02 +0800

    edit

--- cartridge.groovy
+++ cartridge.groovy
@@ -11,7 +11,7 @@ def build_job = folder + "/build-job"
 	
 	configure { project ->
   project / 'properties' / 'hudson.plugins.copyartifact.CopyArtifactPermissionProperty' / 'projectNameList' {
-    'string' "build-job"
+    'string' "ansible-job"
   }
     }
 	triggers{
